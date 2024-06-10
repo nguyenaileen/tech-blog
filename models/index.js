d5cc619cfed310a5ef4484613a9e4907683e2356
Aileen Nguyen <aileenn@gmail.com>
2024-06-10 11:47:56 -0700

    updated the home page and login views

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -11,19 +10,4 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-});
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post };
